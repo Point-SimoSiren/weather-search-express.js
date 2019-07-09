e5768d6bf498e30ef788a32635bb61ae5d2ee4e5
Simo Siren <simo.siren@stud.pointcollege.fi>
2019-07-09 09:38:37 +0300

    asetukset herokua varten

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     viesti2.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + haettava).then((response) => {
+    fetch('/weather?address=' + haettava).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 viesti1.textContent = data.error
